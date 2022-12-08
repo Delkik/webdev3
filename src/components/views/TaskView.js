7ae1c1a2bf1f77089266ed2b19160ebbfd14259e
delkik <danielelkik@hotmail.com>
2022-12-07 23:03:47 -0500

    adds edit view

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const TaskView = (props) => {
   const { task } = props;
-  console.log(task.employee)
   return (
     <div>
       <h1>{task.description}</h1>
-      {task.employee ? <h3>{task.employee.first + " " + task.employee.last}</h3>: <h3>staff</h3>}
-      {/* <Link to={`/edittask/${task.id}`}>Edit task information</Link> */}
+      <h2>Priority: {task.priority}, Status: {task.completion}</h2>
+      {task.employee ? <Link to={`/employee/${task.employeeId}`}><h3>{task.employee.first + " " + task.employee.last}</h3></Link>: <h3>employee</h3>}
+      <Link to={`/edittask/${task.id}`}>Edit task information</Link>
       <br/>
       <Link to={`/tasks`}>View all tasks</Link>
     </div>
--- src/store/thunks.js
+++ src/store/thunks.js
@@ -44,7 +44,6 @@ export const fetchAllTasksThunk = () => async (dispatch) => {
 };
 
 // export const addTaskThunk = (task) => async (dispatch) => {
-//   // task = { title: "CSCI 127" }
 //   try {
 //     let res = await axios.post(`${path}/tasks`, task);
 //     dispatch(ac.addTask(res.data));
@@ -64,15 +63,15 @@ export const fetchAllTasksThunk = () => async (dispatch) => {
 //   }
 // };
 
-// export const editTaskThunk = task => async dispatch => {
-//   try {
-//     let res = await axios.put(`${path}/tasks/${task.id}`, task);
-//     //res.data is the updated task object
-//     dispatch(ac.editTask(res.data));
-//   } catch(err) {
-//     console.error(err);
-//   }
-// };
+export const editTaskThunk = task => async dispatch => {
+  try {
+    let res = await axios.put(`${path}/tasks/${task.id}`, task);
+    //res.data is the updated task object
+    dispatch(ac.editTask(res.data));
+  } catch(err) {
+    console.error(err);
+  }
+};
 
 //Single task
 export const fetchTaskThunk = id => async dispatch => {
